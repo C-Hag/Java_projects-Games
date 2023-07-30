05abe264e766c0b57388341bd45a14ec5519a4bb
C-Hag <c_hagglund@bahnhof.se>
2023-07-30 19:05:12 +0200

    updates

--- Racket/Racket.java
+++ Racket/Racket.java
@@ -1,7 +1,14 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
 public class Racket {
 
     public static void name(String){} args{
         // CREATE AUTOMATIC METHOD SUB 
+
+        PlayFrame frame = new PlayFrame();
         
     }
 }
