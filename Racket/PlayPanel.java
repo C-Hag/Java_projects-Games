c8b83c8e412ba58097be72486cd95aef2ee68981
C-Hag <83981737+C-Hag@users.noreply.github.com>
2023-07-31 17:25:44 +0200

    update

--- Racket/PlayPanel.java
+++ Racket/PlayPanel.java
@@ -38,6 +38,8 @@ public class PlayPanel extends Jpanel implements Runnable {
      }
 
      public void newMovingRackets() {
+        racket1 = new MovingRackets(0,(GAME_HEIGHT/2)-(MovingRackets_HEIGHT/2),MovingRackets_WIDTH,MovingRackets_HEIGHT,1);
+        racket2 = new MovingRackets(GAME_WIDTH-MovingRackets(GAME_HEIGHT/2)-(MovingRackets_HEIGHT/2),MovingRackets_WIDTH,MovingRackets_HEIGHT,1);
 
      }
 
