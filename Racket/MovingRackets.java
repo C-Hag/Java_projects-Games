83493fda5e22bb585d2e4a597b1441449acf4e09
C-Hag <c_hagglund@bahnhof.se>
2023-07-31 20:25:40 +0200

    More graphics

--- Racket/MovingRackets.java
+++ Racket/MovingRackets.java
@@ -8,8 +8,9 @@ public class MovingRackets extends Rectangle {
     int id;
     int yVelocity;
 
-    MovingRackets(){
-
+    MovingRackets(int x, int y, int MovingRackets_WIDTH, int MovingRackets_HEIGHT, int id ){
+        super(x,y,MovingRackets_WIDTH,MovingRackets_HEIGHT);
+        this.id=id;
     }
 
     public void keyPressed(KeyEvent e) {
@@ -29,7 +30,13 @@ public class MovingRackets extends Rectangle {
     }
 
     public void draw(Graphics g) {
+        if(id==1)
+            g.setColor(Color.yellow);
+        else
+            g.setColor(Color.green)
+        g.fillRect(x, y, width, height);    
         
+
     }
 
 }
