caed5925807c8cb0c2a088978feb63d625ef6a92
C-Hag <c_hagglund@bahnhof.se>
2023-07-31 22:57:58 +0200

    Updates

--- Racket/Score.java
+++ Racket/Score.java
@@ -20,6 +20,9 @@ public class Score extends Rectangle{
         g.setFont(new Font("Times New Roman",Font.PLAIN,60));
         
         g.drawLine(GAME_WIDTH/2, 0, GAME_WIDTH/2, GAME_HEIGHT);
+
+        g.drawString(String.valueOf(player1/10)+String.ValueOf(player1/10), (GAME_WIDTH/2)-85, 50);
+        g.drawString(String.valueOf(player2)+String.ValueOf(player2/10), (GAME_WIDTH/2)+20 50);
     }
 
 }
