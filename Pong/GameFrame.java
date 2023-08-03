8b2a62ea4564c9a1cfa24fde8282c31c8e2b7c8d
C-Hag <83981737+C-Hag@users.noreply.github.com>
2023-08-03 16:16:22 +0200

    AI opponent?

--- Pong/GameFrame.java
+++ Pong/GameFrame.java
@@ -1,20 +1,18 @@
 import java.awt.*;
 import javax.swing.*;
 
-public class PlayFrame extends JFrame {
-
-    PlayPanel panel;
-
-    PlayFrame(){
-        panel = new PlayPanel();
+public class GameFrame extends JFrame{
+    GamePanel panel;
+    
+    GameFrame(){
+        panel = new GamePanel();
         this.add(panel);
-        this.setTitle("Racket Game");
+        this.setTitle("Pong Game");
         this.setResizable(false);
         this.setBackground(Color.black);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.pack();
         this.setVisible(true);
         this.setLocationRelativeTo(null);
-    } 
-
-} 
+    }
+}
--- Racket/Racket.java
+++
@@ -1,9 +0,0 @@
-public class Racket {
-
-    public static void main(String[] args){
-        // CREATE AUTOMATIC METHOD SUB 
-
-        PlayFrame frame = new PlayFrame();
-        
-    }
-}
